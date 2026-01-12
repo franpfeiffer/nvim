ad4f019a70eb9fa582528bfcebba6131aa315bc9
Francisco Pfeiffer <franciscopfeiffer.business@gmail.com>
2026-01-12 10:07:45 -0300

    feat?: no tengo idea que hice

--- lua/fran/init.lua
+++ lua/fran/init.lua
@@ -41,7 +41,7 @@ autocmd('BufEnter', {
         if vim.bo.filetype == "zig" then
             pcall(vim.cmd.colorscheme, "rose-pine-moon")
         else
-            pcall(vim.cmd.colorscheme, "kanagawa")
+            pcall(vim.cmd.colorscheme, "tokyonight-night")
         end
     end
 })
